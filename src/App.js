d5bbd329dabcdb8bec033a86869b469d0f1378c5
kirkmonz <kirkemmons2@gmail.com>
2023-01-22 13:04:23 -0500

    color, theme, dark/light mode, & styling setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from "scenes/homePage"
 import LoginPage from "scenes/loginPage"
 import ProfilePage from "scenes/profilePage"
+import { useMemo } from "react"
+import { useSelector } from "react-redux"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import { createTheme } from "@mui/material/styles"
+import { themeSettings } from "theme"
 
 function App() {
+  // `useSelector` grabs the stored value from initialState.mode in "./state/index.js" file.
+  const mode = useSelector((state) => state.mode)
+  // sets theme for Material UI, passed into app.
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          {/* Material UI's version of css reset */}
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   )
