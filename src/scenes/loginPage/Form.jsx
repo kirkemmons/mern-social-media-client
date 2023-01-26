b7d8c3c837f6707db62f79b340e1ed7d2c049107
kirkmonz <kirkemmons2@gmail.com>
2023-01-26 13:13:33 -0500

    add text fields to register form

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -7,14 +7,14 @@ import {
   Typography,
   useTheme,
 } from "@mui/material"
-import EditOutlinedIcon from "@mui/icons-material/EditOutlined"
+// import EditOutlinedIcon from "@mui/icons-material/EditOutlined"
 import { Formik } from "formik"
 import * as yup from "yup"
-import { useNavigate } from "react-router-dom"
-import { useDispatch } from "react-redux"
-import { setLogin } from "state"
-import Dropzone from "react-dropzone"
-import FlexBetween from "components/FlexBetween"
+// import { useNavigate } from "react-router-dom"
+// import { useDispatch } from "react-redux"
+// import { setLogin } from "state"
+// import Dropzone from "react-dropzone"
+// import FlexBetween from "components/FlexBetween"
 
 const registerSchema = yup.object().shape({
   firstName: yup.string().required("required"),
@@ -49,8 +49,8 @@ const initialValuesLogin = {
 const Form = () => {
   const [pageType, setPageType] = useState("login")
   const { palette } = useTheme()
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  // const dispatch = useDispatch()
+  // const navigate = useNavigate()
   const isNonMobile = useMediaQuery("(min-width:600px)")
   const isLogin = pageType === "login"
   const isRegister = pageType === "register"
@@ -72,7 +72,102 @@ const Form = () => {
         handleSubmit,
         setFieldValue,
         resetForm,
-      }) => <form onSubmit={handleSubmit}></form>}
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <Box
+            display="grid"
+            gap="30px"
+            gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+            sx={{
+              "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+            }}
+          >
+            {isRegister && (
+              <>
+                <TextField
+                  label="First Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.firstName}
+                  name="firstName"
+                  error={
+                    Boolean(touched.firstName) && Boolean(errors.firstName)
+                  }
+                  helperText={touched.firstName && errors.firstName}
+                  sx={{ gridColumn: "span 2" }}
+                />
+                <TextField
+                  label="Last Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.lastName}
+                  name="lastName"
+                  error={Boolean(touched.lastName) && Boolean(errors.lastName)}
+                  helperText={touched.lastName && errors.lastName}
+                  sx={{ gridColumn: "span 2" }}
+                />
+                <TextField
+                  label="Location"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.location}
+                  name="location"
+                  error={Boolean(touched.location) && Boolean(errors.location)}
+                  helperText={touched.location && errors.location}
+                  sx={{ gridColumn: "span 4" }}
+                />
+                <TextField
+                  label="Occupation"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.occupation}
+                  name="occupation"
+                  error={
+                    Boolean(touched.occupation) && Boolean(errors.occupation)
+                  }
+                  helperText={touched.occupation && errors.occupation}
+                  sx={{ gridColumn: "span 4" }}
+                />
+              </>
+            )}
+          </Box>
+
+          {/* BUTTONS */}
+          <Box>
+            <Button
+              fullWidth
+              type="submit"
+              sx={{
+                m: "2rem 0",
+                p: "1rem",
+                backgroundColor: palette.primary.main,
+                color: palette.background.alt,
+                "&:hover": { color: palette.primary.main },
+              }}
+            >
+              {isLogin ? "LOGIN" : "REGISTER"}
+            </Button>
+            <Typography
+              onClick={() => {
+                setPageType(isLogin ? "register" : "login")
+                resetForm()
+              }}
+              sx={{
+                textDecoration: "underline",
+                color: palette.primary.main,
+                "&:hover": {
+                  cursor: "pointer",
+                  color: palette.primary.light,
+                },
+              }}
+            >
+              {isLogin
+                ? "Don't have an account? Sign Up here."
+                : "Already have an account? Login here."}
+            </Typography>
+          </Box>
+        </form>
+      )}
     </Formik>
   )
 }
--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -1,5 +1,5 @@
 import { Box, Typography, useTheme, useMediaQuery } from "@mui/material"
-// import Form from "./Form"
+import Form from "./Form"
 
 const LoginPage = () => {
   const theme = useTheme()
@@ -24,14 +24,10 @@ const LoginPage = () => {
         borderRadius="1.5rem"
         backgroundColor={theme.palette.background.alt}
       >
-        <Typography
-          fontWeight="500"
-          variant="h5"
-          sx={{ mb: "1.5rem", textAlign: "center" }}
-        >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
           Welcome to Socipedia, the Social Media for Sociopaths!
         </Typography>
-        {/* <Form /> */}
+        <Form />
       </Box>
     </Box>
   )
