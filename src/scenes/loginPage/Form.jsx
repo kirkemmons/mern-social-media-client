d629b5f8c5d61cd2a68b363e27453e59dd10ca73
kirkmonz <kirkemmons2@gmail.com>
2023-01-26 14:01:20 -0500

    add handleFormSubmit logic

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -10,9 +10,9 @@ import {
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined"
 import { Formik } from "formik"
 import * as yup from "yup"
-// import { useNavigate } from "react-router-dom"
-// import { useDispatch } from "react-redux"
-// import { setLogin } from "state"
+import { useNavigate } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import { setLogin } from "state"
 import Dropzone from "react-dropzone"
 import FlexBetween from "components/FlexBetween"
 
@@ -49,13 +49,58 @@ const initialValuesLogin = {
 const Form = () => {
   const [pageType, setPageType] = useState("login")
   const { palette } = useTheme()
-  // const dispatch = useDispatch()
-  // const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
   const isNonMobile = useMediaQuery("(min-width:600px)")
   const isLogin = pageType === "login"
   const isRegister = pageType === "register"
 
-  const handleFormSubmit = async (values, onSubmitProps) => {}
+  const register = async (values, onSubmitProps) => {
+    // this allows us to send form info with image
+    const formData = new FormData()
+    for (let value in values) {
+      formData.append(value, values[value])
+    }
+    formData.append("picturePath", values.picture.name)
+
+    const savedUserResponse = await fetch(
+      "http://localhost:3001/auth/register",
+      {
+        method: "POST",
+        body: formData,
+      }
+    )
+    const savedUser = await savedUserResponse.json()
+    onSubmitProps.resetForm()
+
+    if (savedUser) {
+      setPageType("login")
+    }
+  }
+
+  const login = async (values, onSubmitProps) => {
+    const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+    })
+    const loggedIn = await loggedInResponse.json()
+    onSubmitProps.resetForm()
+    if (loggedIn) {
+      dispatch(
+        setLogin({
+          user: loggedIn.user,
+          token: loggedIn.token,
+        })
+      )
+      navigate("/home")
+    }
+  }
+
+  const handleFormSubmit = async (values, onSubmitProps) => {
+    if (isLogin) await login(values, onSubmitProps)
+    if (isRegister) await register(values, onSubmitProps)
+  }
 
   return (
     <Formik
