199cc523e4d8a647791ec2973e88d5d7906d7607
kirkmonz <kirkemmons2@gmail.com>
2023-01-26 13:15:42 -0500

    add dropzone functionality to register form

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -7,14 +7,14 @@ import {
   Typography,
   useTheme,
 } from "@mui/material"
-// import EditOutlinedIcon from "@mui/icons-material/EditOutlined"
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined"
 import { Formik } from "formik"
 import * as yup from "yup"
 // import { useNavigate } from "react-router-dom"
 // import { useDispatch } from "react-redux"
 // import { setLogin } from "state"
-// import Dropzone from "react-dropzone"
-// import FlexBetween from "components/FlexBetween"
+import Dropzone from "react-dropzone"
+import FlexBetween from "components/FlexBetween"
 
 const registerSchema = yup.object().shape({
   firstName: yup.string().required("required"),
@@ -128,6 +128,39 @@ const Form = () => {
                   helperText={touched.occupation && errors.occupation}
                   sx={{ gridColumn: "span 4" }}
                 />
+                <Box
+                  gridColumn="span 4"
+                  border={`1px solid ${palette.neutral.medium}`}
+                  borderRadius="5px"
+                  p="1rem"
+                >
+                  <Dropzone
+                    acceptedFiles=".jpg,.jpeg,.png"
+                    multiple={false}
+                    onDrop={(acceptedFiles) =>
+                      setFieldValue("picture", acceptedFiles[0])
+                    }
+                  >
+                    {({ getRootProps, getInputProps }) => (
+                      <Box
+                        {...getRootProps()}
+                        border={`2px dashed ${palette.primary.main}`}
+                        p="1rem"
+                        sx={{ "&:hover": { cursor: "pointer" } }}
+                      >
+                        <input {...getInputProps()} />
+                        {!values.picture ? (
+                          <p>Add Picture Here</p>
+                        ) : (
+                          <FlexBetween>
+                            <Typography>{values.picture.name}</Typography>
+                            <EditOutlinedIcon />
+                          </FlexBetween>
+                        )}
+                      </Box>
+                    )}
+                  </Dropzone>
+                </Box>
               </>
             )}
           </Box>
